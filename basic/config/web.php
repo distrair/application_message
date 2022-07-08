09c4c30aab99ca3f742137b6ee36540a349c9323
JloMkAaA <vladislav.tyazhelov5@gmail.com>
2022-07-08 22:24:51 +0300

    123
    123

--- basic/config/web.php
+++ basic/config/web.php
@@ -3,7 +3,6 @@
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
-
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
@@ -15,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '666666777',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -44,14 +43,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        
+        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        
+        */
     ],
     'params' => $params,
 ];
