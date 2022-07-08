109dfe75311bdf8bde0b8380444d482d7c5390c6
JloMkAaA <vladislav.tyazhelov5@gmail.com>
2022-07-08 22:16:40 +0300

    Update index.php

--- application_message/views/site/index.php
+++ application_message/views/site/index.php
@@ -1,8 +1,13 @@
+<?= 
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+?>
+
 <html>
     <head>
 
     </head>
-    
+
     <body>
         <p>123</p>
     </body>
