28ec1866fd831cb2b9beafbc8a1abf60b1b9af2e
magistorS <96631431+magistorS@users.noreply.github.com>
2022-07-09 15:42:57 +0300

    Update login.php

--- basic/views/site/login.php
+++ basic/views/site/login.php
@@ -25,7 +25,14 @@ $this->params['breadcrumbs'][] = $this->title;
     ]); ?>
 
     <div>
-    <h1><span>I</span>n<span>tersection</span></h1>
+    <h1><span>I</span><span>ntersection</span></h1>
+    <div class="wrapper">
+    <svg width="200" height="200">
+         <circle cx="80" cy="80" r="80" fill="black" />
+        <circle class="bounce" cx="80" cy="55" r="15" fill="#e52e71" />
+        <rect x="65" y="80" width="28" height="50" fill="#ff8a00" />
+    </svg>
+</div>
 <style>
 
 h1{
@@ -40,10 +47,17 @@ h1 span{
   transition: .5s linear
 }
 h1:hover span:nth-child(1){
-  margin-right: 5px
+  margin-right: 5px;
+ color: red;
+}
+
+h1:hover span::after {
+    color: #fff;
+
+  
 }
 h1:hover span:nth-child(1):after{
-
+ 
 }
 h1:hover span:nth-child(2){
   margin-left: 30px
@@ -51,8 +65,28 @@ h1:hover span:nth-child(2){
 h1:hover span{
   color: #fff;
   text-shadow: 0 0 10px #000,
-               0 0 20px #fff, 
-               0 0 40px #fff;
+               0 0 20px #000, 
+               0 0 40px #000;
+}
+.wrapper {
+    margin: 2em;
+    display: flex;
+    justify-content: center;
+}
+
+@keyframes bounce {
+    from {
+        transform: translate3d(0, 0, 0);
+    }
+    to {
+        transform: translate3d(0, -15px, 0);
+    }
+}
+
+.bounce {
+    animation: bounce 0.4s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
 }
 </style>
     </div>
